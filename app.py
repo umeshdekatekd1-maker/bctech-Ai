34ced613c0e3c6e9c3161e42f3a10857018e6329
umeshdekatekd1-maker <umeshdekatekd1@gmail.com>
2026-09-06 18:25:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ if st.button("Ask AI (Puchhein)"):
                 Question: {user_query}
                 """
                 response = client.models.generate_content(
-                    model="gemini-2.0-flash",
+                    model="gemini-3.6-flash",
                     contents=prompt
                 )
                 st.success("AI Jawab:")
